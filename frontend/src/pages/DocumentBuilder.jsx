03201ed85dbe663a5337a57cb5f0fa9177bd8e11
Stepanov Artur <89976322+satret@users.noreply.github.com>
2026-04-15 10:31:17 +0300

    Update DocumentBuilder.jsx

--- frontend/src/pages/DocumentBuilder.jsx
+++ frontend/src/pages/DocumentBuilder.jsx
@@ -654,7 +654,7 @@ export default function DocumentBuilder({ subsection, onPageChange }) {
             <div className="doc-builder-resource-item">
               <h5>Хотите действовать через представителя?</h5>
               <button type="button" className="doc-builder-tip-link" onClick={goToRepresentativeHelp}>
-                Узнайте как это сделать в разделе "Алгоритм судебного сопровождения"
+                Узнайте как это сделать в разделе "Юридический навигатор"
               </button>
             </div>
             <div className="doc-builder-resource-item">
